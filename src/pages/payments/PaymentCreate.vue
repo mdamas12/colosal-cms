b14ee73cfbbf4d5c7d7568f515fa1ecbfacffe5e
Eduardo Hernandez <eduardo.hernandez.puga@gmail.com>
2021-03-01 09:25:21 -0400

    haciendo push nuevamente

---
+++ src/pages/payments/PaymentCreate.vue
@@ -0,0 +1,406 @@
+<template>
+  <q-page>
+      <div class="col-md-12 col-xs-12 q-pa-sm">
+
+        <div class="q-ma-sm">
+          <div class="row">
+            <div class="col">
+              <div class="row">
+                <div class="col" style="max-width:40px;">
+                  <q-btn flat round icon="keyboard_backspace" style="color:#9E9E9E" @click="$router.back()" />
+                </div>
+                <div class="col2">
+                  <h5 class="vertical-top col2 text-indigo-10 text-weight-bolder q-pa-sm" style="margin-top:-3px">
+                      Nueva Venta
+                  </h5>
+                </div>
+              </div>
+
+              <div class="row">
+                <h6 class="vertical-top col2 text-indigo-10 text-weight-bolder q-pa-sm" style="margin-top:-3px">
+                    Información de Compra
+                </h6>
+              </div>
+              
+            </div>
+            <div class="column items-end">
+              <div class="col">
+                <div class="row">
+                  <q-btn color="red-10" label="Crear compra" class="q-pa-xs float-right" @click="createPayment()">
+                    <template v-slot:loading>
+                      <q-spinner-facebook /> 
+                    </template>
+                  </q-btn>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="q-ma-sm">
+            <div class="row q-ma-bg">
+              <div class="col">               
+                <q-select
+                  outlined
+                  use-input
+                  hide-selected
+                  fill-input
+                  input-debounce="0"
+                  v-model="clientNameModel"
+                  label="Cliente"
+                  stack-label
+                  :options="options"
+                  @filter="filterFn"
+                >
+                  <template v-slot:control>
+                    <div class="self-center full-width no-outline" tabindex="0"></div>
+                  </template>
+                </q-select>           
+              </div>
+              <div class="col q-mx-sm">               
+                <q-input  
+                  outlined
+                  v-model="payment.description"
+                  autogrow
+                  label="Descripción"
+                />
+              </div>
+            </div>
+            <br/>
+            <q-separator inset class="q-my-md"/>
+            <div class="row q-ma-bg">
+              <div class="col2">
+                <h6 class="vertical-top col2 text-indigo-10 text-weight-bolder q-pa-sm" style="margin-top:-3px">
+                    Información de Pago
+                </h6>
+              </div>
+            </div>
+
+            <div class="row q-ma-bg">
+              <div class="col">               
+                <q-select
+                  outlined
+                  use-input
+                  hide-selected
+                  fill-input
+                  input-debounce="0"
+                  v-model="bankNameModel"
+                  label="Banco"
+                  stack-label
+                  :options="options3"
+                  @filter="filterFn3"
+                >
+                  <template v-slot:control>
+                    <div class="self-center full-width no-outline" tabindex="0"></div>
+                  </template>
+                </q-select>              
+              </div>
+              <div class="col q-mx-sm">               
+                <q-select
+                  outlined
+                  v-model="payment.coin"
+                  :options="currencyOptions"
+                  label="Moneda"
+                  stack-label
+                >
+                  <template v-slot:prepend>
+                    <q-icon name="money" />
+                  </template>
+                </q-select>           
+              </div>
+              <div class="col">               
+                <q-select
+                  outlined
+                  v-model="payment.payment_type"
+                  :options="paymentOptions"
+                  label="Forma de Pago"
+                  stack-label
+                >
+                </q-select>           
+              </div>
+            </div>
+            <br/>
+            <q-separator inset class="q-my-md"/>
+            <div class="row q-ma-bg">
+              <div class="col2">
+                <h6 class="vertical-top col2 text-indigo-10 text-weight-bolder q-pa-sm" style="margin-top:-3px">
+                    Productos Adquiridos
+                </h6>
+              </div>
+            </div>
+
+            <div class="row q-mx-md">
+              <div class="col">
+                <div v-for="(item, index) in this.payment_detail">
+                  <div class="row q-mb-md">
+                    <div class="col-8">
+                      <q-select
+                        outlined
+                        use-input
+                        hide-selected
+                        fill-input
+                        v-model="productNameModel[index]"
+                        input-debounce="0"
+                        label="Producto"
+                        :options="options2"
+                        @filter="filterFn2"
+                      >
+                        <template v-slot:no-option>
+                          <q-item>
+                            <q-item-section class="text-grey">
+                              No results
+                            </q-item-section>
+                          </q-item>
+                        </template>
+                      </q-select>
+                    </div>
+                    <div class="col-2 q-mx-auto">
+                      <q-input
+                        type="number"
+                        v-model.number="payment_detail[index].quantity"
+                        label="Cantidad"
+                        outlined
+                      />
+                    </div>
+                    <div class="col-1 self-center">
+                      <div class="row">
+                        <q-btn flat round color="indigo-10" icon="delete" @click="this.removeProduct(index)"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <q-btn flat round color="indigo-10" icon="add" @click="addProduct()"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </q-page>
+</template>
+
+<script>
+import Vue from 'vue'
+import PaymentsService from '../../services/payments/payments.service'
+import ClientsService from '../../services/customers/customers.service'
+import ProductsService from '../../services/products/products.service'
+import PaymentsService from '../../services/payments/payments.service'
+import { Loading } from 'quasar';
+
+export default Vue.extend({
+  data () {
+    return {
+      paymentOptions: ['ZELLE', 'TRANSFERENCIA BS', 'TRANSFERENCIA $', 'PAGO MOVIL', 'EFECTIVO'],
+      currencyOptions: ["USD", "BS"],
+      loading: false,
+      payment: {
+        description: "",
+        customer: 0,
+        bank: 0,
+        payment_type: "",
+        coin : "",
+      },
+      payment_detail:[{
+        product: 0,
+        price: 0,
+        quantity: 0,
+      }],
+      selectedProducts: [{
+        id: 0,
+        label: '',
+        value: '',
+        name: '',
+        price: 0
+      }],
+      clientOptions: [{
+        id: 0,
+        name: ''
+      }],
+      bankOptions: [{
+        id: 0,
+        name: '',
+      }],
+      productOptions: [{
+        id: 0,
+        name: '',
+        price: 0
+      }],
+      clientIndex: [],
+      options: [],
+      productIndex: [],
+      productPrices: [],
+      options2: [],
+      clientNameModel: "",
+      productNameModel: [],
+      bankIndex: [],
+      options3: [],
+      bankNameModel: ""
+    }
+  },
+  mounted(){
+    let vm = this;
+    vm.onRequest();
+  },
+  methods: {
+    onRequest(){
+      // Loading.show();
+      let subscription = ClientsService.getCustomers().subscribe({
+        next: data => {
+          this.clientOptions.pop();
+          for (let i = 0; i < data.results.length; i++) {
+            this.clientOptions.push(data.results[i].fullname);
+            this.clientIndex.push(data.results[i].id);
+          }
+        },
+        complete: () => {
+          console.log('[complete]');
+        }
+      });
+      
+      let subscription2 = ProductsService.getAllProducts().subscribe({
+        next: data2 => {
+          this.productOptions.pop();
+          for (let i = 0; i < data2.results.length; i++) {
+            this.productOptions.push(data2.results[i].name);
+            this.productIndex.push(data2.results[i].id);
+            this.productPrices.push(data2.results[i].price);
+          }
+        },
+        complete: () => {
+          console.log('[complete]');
+        }
+      });
+
+      let subscription3 = PaymentsService.getPayments().subscribe({
+        next: data3 => {
+          this.bankOptions.pop();
+          for (let i = 0; i < data3.length; i++) {
+            this.bankOptions.push(data3[i].name);
+            this.bankIndex.push(data3[i].id);
+          }
+        },
+        complete: () => {
+          console.log('[complete]');
+          // Loading.hide();
+        }
+      });
+    },
+    addProduct(){
+      this.payment_detail.push({
+        product: 0,
+        price: 0,
+        quantity: 0
+      });
+      this.productNameModel.push("");
+    },
+    removeProduct(index){
+      if (this.payment_detail.length > 1){
+        this.payment_detail.splice(index,1);
+        this.productNameModel.splice(index,1);
+      }
+    },
+    createPayment(){
+      if (this.clientNameModel === ""){
+        this.showNotif("Proveer nombre de cliente", 'red-10');
+        return;
+      };
+      if (this.payment.coin === "" || this.payment.payment_type === ""){
+        this.showNotif("Completar información de pago", 'red-10');
+        return;
+      };
+      if (this.payment_detail.length == 0){
+        this.showNotif("Debe agregar al menos un producto", 'red-10');
+        return;
+      };
+      for (var i=0; i < this.selectedProducts.length; i++) {
+        if (this.productNameModel[i] === ""){
+          this.showNotif(`Falta información del producto ${i}`, 'red-10');
+          return;
+        }
+        if (this.payment_detail[i].quantity <= 0){
+          this.showNotif(`Especificar cantidad en ${this.selectedProducts[i].name}`, 'red-10');
+          return;
+        }
+      };
+      if (this.bankNameModel === ""){
+        this.showNotif("Proveer nombre de banco", 'red-10');
+        return;
+      };
+      this.payment.customer = this.clientOptions.indexOf(this.clientNameModel) >= -1? this.clientIndex[this.clientOptions.indexOf(this.clientNameModel)]: null;
+      this.payment.bank = this.bankOptions.indexOf(this.bankNameModel) >= -1? this.bankIndex[this.bankOptions.indexOf(this.bankNameModel)]: null;
+
+      for (let i = 0; i < this.payment_detail.length; i++) {
+        this.payment_detail[i].product = this.productOptions.indexOf(this.productNameModel[i]) >= -1? this.productIndex[this.productOptions.indexOf(this.productNameModel[i])]: null; 
+        this.payment_detail[i].price = this.productOptions.indexOf(this.productNameModel[i]) >= -1? this.productPrices[this.productOptions.indexOf(this.productNameModel[i])]: null; 
+      }
+      console.log("everything in order. Creating payment...");
+      this.loading = true;
+      let subscription = PaymentsService.createPayment({payment: this.payment, payment_detail: this.payment_detail}).subscribe({
+        complete: () =>{
+          this.loading = false;
+          this.showNotif("Venta creada exitosamente", 'indigo-10');
+          setTimeout(backToPayments(),1000);
+        }
+      });
+    },
+    showNotif (message, color) {
+      this.$q.notify({
+        message: message,
+        color: color,
+        actions: [
+          { label: 'Dismiss', color: 'white', handler: () => { /* ... */ } }
+        ]
+      })
+    },
+    filterFn (val, update) {
+      // call abort() at any time if you can't retrieve data somehow
+      // setTimeout(() => {
+        update(() => {
+          if (val === '') {
+            this.options = this.clientOptions;
+          }
+          else {
+            const needle = val.toLowerCase()
+            this.options = this.clientOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+            console.log("val: "+val)
+          }
+          
+        })
+      // }, 1500)
+    },
+    filterFn2 (val, update) {
+      // call abort() at any time if you can't retrieve data somehow
+      // setTimeout(() => {
+        update(() => {
+          if (val === '') {
+            this.options2 = this.productOptions;
+          }
+          else {
+            const needle = val.toLowerCase()
+            this.options2 = this.productOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+            console.log("val: "+val)
+          }
+          
+        })
+      // }, 1500)
+    },
+    filterFn3 (val, update) {
+      // call abort() at any time if you can't retrieve data somehow
+      // setTimeout(() => {
+        update(() => {
+          if (val === '') {
+            this.options3 = this.bankOptions;
+          }
+          else {
+            const needle = val.toLowerCase()
+            this.options3 = this.bankOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+            console.log("val: "+val)
+          }
+          
+        })
+      // }, 1500)
+    },
+    backToPayments(){
+      this.$router.push({path:"payments/"});
+    }
+  }
+})
+</script>
---
+++ src/pages/payments/PaymentList.vue
@@ -0,0 +1,121 @@
+<template>
+  <q-page>
+    <div class="row q-pa-sm q-pl-lg q-pr-lg">
+      <div class="col-md-12 col-xs-12 q-pt-lg">
+        <div class="row col-xs-12 q-pb-xl">
+          <div class="col">
+            <h5 class="text-indigo-10 text-weight-bolder q-ma-none">
+              Promociones
+            </h5>
+            <small class="text-subtitle2 text-grey-7 q-mb-none" >Existen {{this.count}} promociones almacenadas</small>
+            <!--<q-skeleton v-else type="text" width="50%" animation="fade" />-->
+          </div>
+          <div class="col">
+              <q-btn color="red-10" label="Crear promoción" class="float-right" @click="$router.push({ name : 'PaymentCreate'/* , params : {contact : contact }  */})"/>
+          </div>
+        </div>
+
+        <div class="col-md-12 col-xs-12 q-pt-lg">
+            <q-table
+              :loading="loading"
+              :data="this.rows"
+              :columns="columns"
+              row-key="name"
+              :pagination.sync="pagination"
+              hide-pagination
+              @row-click="onRowClick"
+            />
+
+          <div class="row justify-center q-mt-md" v-if="numberOfPages > 1">
+            <q-pagination
+              v-model="pagination.page"
+              color="indigo-10"
+              :max="this.numberOfPages"
+              size="sm"
+              @click="onRequest()"  
+            />
+          </div>
+        </div>       
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { Loading } from 'quasar'
+import Vue from 'vue'
+import PaymentsService from '../../services/payments/payments.service'
+
+export default Vue.extend({
+  meta: {
+    title: 'Categories - List'
+  },
+  data(){    
+    return {
+      loading: false,
+      pagination: {
+        // sortBy: 'desc',
+        // descending: false,
+        page: 1,
+        rowsPerPage: 25,
+        rowsNumber: 0
+      },
+      table: [0], 
+      limit: 25,
+      offset: 0,
+      count: 0,
+      currentPage: 1,
+      numberOfPages: 0,
+      columns: [
+        { name: 'name', align: 'left', label: 'Nombre', field: 'name', sortable: false/*, headerClasses: 'bg-red-10 text-white', classes: 'bg-red-1 text-dark ellipsis' */},
+        { name: 'description', align: 'left', label: 'Descripción', field: 'description', sortable: false/*, headerClasses: 'bg-red-10 text-white', classes: 'bg-red-1 text-dark ellipsis' */},
+        { name: 'price', align: 'left', label: 'Precio', field: 'price', sortable: false/*, headerClasses: 'bg-red-10 text-white', classes: 'bg-red-1 text-dark ellipsis' */},
+        { name: 'coin', align: 'left', label: 'Moneda', field: 'coin', sortable: false/*, headerClasses: 'bg-red-10 text-white', classes: 'bg-red-1 text-dark ellipsis' */},
+        { name: 'quantity', align: 'left', label: 'Existencias', field: 'quantity', sortable: false/*, headerClasses: 'bg-red-10 text-white', classes: 'bg-red-1 text-dark ellipsis' */},
+      ],
+      rows: [],
+      payment: {
+        name: "",
+        description: "",
+        image: "",
+        price: 0,
+        coin: "",
+        quantity: 0,
+      },
+    }
+  },
+  mounted () {
+    const vm = this;
+    vm.onRequest();
+    vm.pagination.rowsNumber = vm.count;
+  },
+  methods: {
+    onRequest(){
+      this.loading = true;
+      console.log("pagination.page == "+ this.pagination.page);
+      this.pagination.currentPage = this.pagination.page;
+      this.offset = this.limit * (this.pagination.page - 1);
+      this.table.splice(0,1);
+      console.log(this.table);
+      this.table.splice(0,0,1);
+      console.log(`PaymentsService.getPayments(limit: ${this.limit}, this.pagination.offset: ${this.offset})`);
+      let subscription = PaymentsService.getPayments(this.limit, this.offset).subscribe({
+        next: data => {
+          console.log(data)
+          this.rows.splice(0, this.rows.length, ...data.results);
+          console.log(this.rows)
+          this.count = data.count
+          this.numberOfPages = Math.ceil(this.count / this.limit);
+          this.loading = false;
+        },
+        complete: () => console.log('[complete]'),
+      })
+      
+    },
+    onRowClick (evt, row){
+      //console.log(`/payments/detail/${row.id}`);
+      this.$router.push({path: `/payments/detail/${row.id}`})
+    },
+  },
+})
+</script>
