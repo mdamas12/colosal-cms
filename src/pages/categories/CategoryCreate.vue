27e9ed7e1d182f7e1c894527f22c983af08e1a3d
Eduardo Hernandez <eduardo.hernandez.puga@gmail.com>
2021-03-10 22:53:02 -0400

    categorías v2

--- src/pages/categories/CategoryCreate.vue
+++ src/pages/categories/CategoryCreate.vue
@@ -3,48 +3,60 @@
       <div class="col-md-12 col-xs-12 q-pa-sm">
 
         <div class="q-pa-sm">
-            <div class="row">
-                <div class="col2">
-                    <q-btn flat round icon="keyboard_backspace" style="color:#9E9E9E" @click="$router.back()" />
-                </div>
+          <div class="row">
+              <div class="col" style="max-width:40px;">
+                  <q-btn flat round icon="keyboard_backspace" style="color:#9E9E9E" @click="$router.back()" />
+              </div>
+              <div class="col2">
                 <h5 class="vertical-top col2 text-indigo-10 text-weight-bolder q-pa-sm" style="margin-top:-3px">
                     Crear Categoría
                 </h5>
+              </div>
+              <div class="col">
+                <q-btn color="red-10" label="Crear Categoría" class="q-pa-xs q-mt-xs q-mr-md float-right" @click="createCategory()"/>
+              </div>
+          </div>
+
+          <div class="q-ma-md">
+            <div class="row justify-between items-center">
+              <div class="col-9 justify-evenly">
+                <div class="row q-my-sm">
+                  <q-input  
+                    outlined
+                    v-model="category.name"
+                    label="Nombre"
+                    color="dark"
+                  />
+                </div>
+                <div class="row items-center q-my-sm">
+                  <q-file 
+                    outlined 
+                    clearable 
+                    counter 
+                    v-model="category.image"        
+                    :filter="checkFileType"
+                    @rejected="onRejected"
+                    @input="getImage"
+                  >
+                    <template v-slot:prepend>
+                      <q-icon name="attach_file" />
+                    </template>
+                    
+                    <template v-slot:hint>
+                      Field hint
+                    </template>
+                  </q-file>
+                </div>
+
+              </div>
+              <div class="col-2">
+                <div v-if="category.image !== null">
+                  <q-img :src="preview" style="max-width: 150px" />
+                </div>
+              </div>
             </div>
-
-            <div class="q-ma-md">
-              <div class="row q-my-sm">
-                <q-input  
-                  outlined
-                  v-model="category.name"
-                  label="Nombre"
-                  lazy-rules
-                  color="dark"
-                />
-              </div>
-              <div class="row q-my-sm">
-                <q-file 
-                  outlined 
-                  clearable 
-                  counter 
-                  v-model="category.image"        
-                  :filter="checkFileType"
-                  @rejected="onRejected"
-                >
-                  <template v-slot:prepend>
-                    <q-icon name="attach_file" />
-                  </template>
-                  
-                  <template v-slot:hint>
-                    Field hint
-                  </template>
-                </q-file>
-              </div>
-            </div>  
-            <q-btn color="red-10" label="Crear Categoría" class="q-pa-xs q-mt-md q-mr-md float-right" @click="createCategory()"/>
-
+          </div>
         </div>
-
       </div>
   </q-page>
 </template>
@@ -61,6 +73,7 @@ export default Vue.extend({
         name: '',
         image: null,
       },
+      preview: null,
       uploading: null
     }
   },
@@ -68,7 +81,7 @@ export default Vue.extend({
   computed: {
     isUploading () {
       return this.uploading !== null
-    },
+    }
   },
   methods: {
     createCategory(){
@@ -87,7 +100,13 @@ export default Vue.extend({
     checkFileType (files) {
       return files.filter(file => file.type === 'image/png')
     },
-
+    getImage(e){
+      let reader = new FileReader();
+      reader.readAsDataURL(e);
+      reader.onload = e => {
+        this.preview = e.target.result;
+      }
+    },
     onRejected (rejectedEntries) {
       // Notify plugin needs to be installed
       // https://quasar.dev/quasar-plugins/notify#Installation
